28566f902b8d5ebca8aef532d7d39fcf8f691598
1902296058@qq.com <1902296058@qq.com>
2025-04-18 18:57:01 +0800

    加入项目文件

---
+++ MDK-ARM/RTE/_diansai666/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'diansai666' 
+ * Target:  'diansai666' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
--- README.md
+++
@@ -1,2 +0,0 @@
-# supreme-octo-happiness
-用来练习git的使用，同时学习如何多人合作开发
